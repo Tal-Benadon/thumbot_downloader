1e9b78f2fce9d2f06828c34bee29a29a042d8222
Tal Ben Adon <talbenadon2@gmail.com>
2025-02-18 17:29:01 +0900

    structure save

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -30,16 +30,22 @@ def extract_metadata_info(url: str) -> Dict[str, Any]:
 
 
 # Checks for the different formats and sizes existing in the given metadata
-def metadata_format_info(info_dict):
-    formats_info = []
+def metadata_formats_info(info_dict):
+    formats_info = {}
     if 'formats' in info_dict:
         for format in info_dict['formats']:
+            
             format_id = format.get('format_id')
+            if not format_id:
+                continue
+            
             format_ext = format.get('ext', 'Unknown extension')
             format_note = format.get('format_note', 'Unknown note')
+            format_url = format.get('url', None)
             filesize = format.get('filesize', None)
             filesize_approx = format.get('filesize_approx', None) 
             resulution = format.get('resolution', None)
+            
             
             # size readablity
             if filesize is not None:
@@ -47,18 +53,22 @@ def metadata_format_info(info_dict):
             if filesize_approx is not None:
                 filesize_approx = round(int(filesize_approx) / (1024 * 1024),2)
                 
-            formats_info.append({
-                'format_id': format_id,
+            formats_info[format_id] = {
                 'format_ext': format_ext,
                 'format_note':format_note,
+                # 'format_url':format_url,
                 'filesize': filesize,
                 'filesize_approx':filesize_approx,
                 'resolution': resulution
-            })
+            }
     pprint.pprint(formats_info)
     return formats_info
 
+
+
+def choose_facebook_format(formats_info: Dict[str, Any]) -> str:
     
+    return
     
     
         
@@ -83,11 +93,11 @@ def check_video_size(video_url: str, max_size=15_728_640)-> int:
 
 
 
-def download_video(url: str) -> str:
+def download_video(url: str, format_url: str) -> str:
     video_path_template = os.path.join(video_dir,'%(title)s.%(ext)s')
     
     ydl_opts: Dict[str, Any] = {
-         'format': "dash-1660324924885002vd",
+        #  'format': format_url,
         'outtmpl': video_path_template
         }
     
@@ -104,10 +114,10 @@ def download_video(url: str) -> str:
 
 
 def temp_main():
-    initial_link = "https://www.instagram.com/reel/DFFcn61qQ1x/?igsh=MXN4c2txNTA5eHdzYQ=="
+    initial_link = "https://www.reddit.com/r/EscapefromTarkov/comments/1irbvdr/is_this_pretty_much_everyones_experience_this/"
     info_dict = extract_metadata_info(initial_link)
-    # print(info_dict)
-    metadata_format_info(info_dict)
+    print(info_dict)
+    metadata_formats_info(info_dict)
     
     
     video_url = info_dict.get('url')
@@ -120,12 +130,12 @@ temp_main()
 
 
 #// ********** working size checks links list **************//#
-# https://www.facebook.com/share/r/1X9i6Htg4r/ <- 9mb video, has av1 reduced to 500kb          
-# https://www.instagram.com/reel/DFFcn61qQ1x/?igsh=MXN4c2txNTA5eHdzYQ==
-# https://www.facebook.com/share/v/1A1tYB3oGk/ <- big video, has av1 that reduced it form 180 to 16
-# 
-# 
-# 
+# https://www.facebook.com/share/r/1X9i6Htg4r/ <- 9mb video, has av1 reduced to 500kb, has 720x has 1080x       
+# https://www.instagram.com/reel/DFFcn61qQ1x/?igsh=MXN4c2txNTA5eHdzYQ== has 720x has 1080x 
+# https://www.facebook.com/share/v/1A1tYB3oGk/ <- big video, has av1 that reduced it form 180 to 16 has 720x has 1080x 
+# https://www.facebook.com/reel/1690243335234089 <- similar to the othe fb links
+# https://www.facebook.com/share/v/14r6jGK6jgX/ <- has ONLY 480x854 res
+# https://www.facebook.com/share/v/1ZMgYEge4q/ <- has ONLY 640x640 res
 # 
 # 
 # 
