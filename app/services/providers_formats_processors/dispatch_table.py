ee0d71cd601b2781f5b447a572b0d0fb0ae3fd3f
Tal Ben Adon <talbenadon2@gmail.com>
2025-02-26 18:05:24 +0900

    instead of optimization, we discovered tiktok does not need support (as non tiktok users, we didnt really try posting its videos before), therefore, support is deleted

--- app/services/providers_formats_processors/dispatch_table.py
+++ app/services/providers_formats_processors/dispatch_table.py
@@ -1,14 +1,12 @@
 from .facebook import choose_facebook_format
 from .instagram import choose_instagram_format
 from .reddit import choose_reddit_format
-from .tiktok import choose_tiktok_format
 
 dispatch_table = {
         'facebook': choose_facebook_format,
         'fb.watch': choose_facebook_format,
         'instagram': choose_instagram_format,
         'reddit': choose_reddit_format,
-        'tiktok': choose_tiktok_format
     }
 
 
