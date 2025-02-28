14088c5187406d98656a2584c8e97827cb1025c2
Tal Ben Adon <talbenadon2@gmail.com>
2025-03-01 00:59:19 +0900

    added exception to error handling in facebook.py

--- app/services/providers_formats_processors/facebook.py
+++ app/services/providers_formats_processors/facebook.py
@@ -12,13 +12,14 @@ def choose_facebook_format(formats_info: Dict[str, Any]) -> str: #is listed per 
     # pprint.pprint(filtered_formats_info)
     # TODO: Add flag to flip priority to HD
     # we prioritize sd for less bandwith
-    for priority in ["sd", "hd"]:                                
-        if priority in filtered_formats_info:
-            chosen_format = {
-                'format_id':priority, 
-                'format_url':filtered_formats_info[priority].get('format_url')
-            }
-            break # will break if sd is found
+    try:
+        for priority in ["sd", "hd"]:                                
+            if priority in filtered_formats_info:
+                chosen_format = {
+                    'format_id':priority, 
+                    'format_url':filtered_formats_info[priority].get('format_url')
+                }
+                break # will break if sd is found
         
         
     # if sd nor hd are not found, try to salvage something with resolution without av01
@@ -34,6 +35,8 @@ def choose_facebook_format(formats_info: Dict[str, Any]) -> str: #is listed per 
     #             break
     
     # if still None
-    if not chosen_format:
-        raise NoSupportedFormatAvailable("avalible format not found")
-    return chosen_format
+        if not chosen_format:
+            raise NoSupportedFormatAvailable("avalible format not found")
+    except NoSupportedFormatAvailable as e:
+        return{"Error": {"FacebookError": e}}
+        return chosen_format
